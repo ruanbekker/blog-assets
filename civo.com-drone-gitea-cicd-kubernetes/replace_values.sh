a02c2ef5024de9f3ccf6121b2084b44dd8f98e0b
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-12-10 08:32:53 +0200

    Update replace_values.sh

--- civo.com-drone-gitea-cicd-kubernetes/replace_values.sh
+++ civo.com-drone-gitea-cicd-kubernetes/replace_values.sh
@@ -1,8 +1,47 @@
 #!/usr/bin/env bash
 
-for file in $(find manifests/ -name "*.y*ml")
-do
-  sed -i "s/\$CHANGEME_DRONERPCSECRET/$CHANGEME_DRONERPCSECRET/g" ${file}
-  sed -i "s/\$CHANGEME_INGRESSENDPOINT/$CHANGEME_INGRESSENDPOINT/g" ${file}
-  sed -i "s/\$CHANGEME_GITEASECRETID/$CHANGEME_GITEASECRETID/g" ${file}
-done
+check_gitea_environment(){
+  if [ -z ${CHANGEME_POSTGRESSPASSWORD} ] || [ -z ${CHANGEME_INGRESSDOMAIN} ] 
+  then
+    echo "Requires Environment Variables:"
+    echo "CHANGEME_POSTGRESSPASSWORD"
+    echo "CHANGEME_INGRESSDOMAIN"
+    exit 1
+  fi
+}
+
+check_drone_environment(){
+  if [ -z ${CHANGEME_DRONEUSER} ] || [ -z ${CHANGEME_POSTGRESSPASSWORD} ] || [ -z ${CHANGEME_INGRESSDOMAIN} ] || [ -z ${CHANGEME_DRONERPCSECRET} ] || [ -z ${CHANGEME_DRONECLIENTID} ] || [ -z ${CHANGEME_DRONECLIENTSECRET} ] || [ -z ${CHANGEME_DRONERPCSECRET} ] 
+  then
+    echo "Requires Environment Variables:"
+    echo "CHANGEME_DRONEUSER"
+    echo "CHANGEME_POSTGRESSPASSWORD"
+    echo "CHANGEME_INGRESSDOMAIN"
+    echo "CHANGEME_DRONERPCSECRET"
+    echo "CHANGEME_DRONECLIENTID"
+    echo "CHANGEME_DRONECLIENTSECRET"
+    echo "CHANGEME_DRONERPCSECRET"
+    exit 1
+  fi
+}
+
+if [ ${1} == "gitea" ] 
+then
+  check_gitea_environment
+  sed -i "s/\$CHANGEME_POSTGRESSPASSWORD/$CHANGEME_POSTGRESSPASSWORD/g" manifests/postgres.yml
+  sed -i "s/\$CHANGEME_POSTGRESSPASSWORD/$CHANGEME_POSTGRESSPASSWORD/g" manifests/gitea.yml
+  sed -i "s/\$CHANGEME_INGRESSDOMAIN/$CHANGEME_INGRESSDOMAIN/g" manifests/gitea.yml
+  sed -i "s/\$CHANGEME_INGRESSDOMAIN/$CHANGEME_INGRESSDOMAIN/g" manifests/ingress.yml
+fi
+
+if [ ${1} == "drone" ] 
+then
+  check_drone_environment
+  sed -i "s/\$CHANGEME_DRONEUSER/$CHANGEME_DRONEUSER/g" manifests/drone-server.yml
+  sed -i "s/\$CHANGEME_POSTGRESSPASSWORD/$CHANGEME_POSTGRESSPASSWORD/g" manifests/drone-server.yml
+  sed -i "s/\$CHANGEME_INGRESSDOMAIN/$CHANGEME_INGRESSDOMAIN/g" manifests/drone-server.yml
+  sed -i "s/\$CHANGEME_DRONERPCSECRET/$CHANGEME_DRONERPCSECRET/g" manifests/drone-server.yml
+  sed -i "s/\$CHANGEME_DRONECLIENTID/$CHANGEME_DRONECLIENTID/g" manifests/drone-server.yml
+  sed -i "s/\$CHANGEME_DRONECLIENTSECRET/$CHANGEME_DRONECLIENTSECRET/g" manifests/drone-server.yml
+  sed -i "s/\$CHANGEME_DRONERPCSECRET/$CHANGEME_DRONERPCSECRET/g" manifests/drone-agent.yml
+fi
